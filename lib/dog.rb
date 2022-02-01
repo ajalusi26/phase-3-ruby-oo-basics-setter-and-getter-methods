8c00d801009bea5cca4a1da5b9243872bdeeb27c
AJ Jalusi <ajalusi26@gmail.com>
2022-02-01 11:30:04 -0500

    commit tree 270c3ffa27762cb2ae6365c299c2b03e43544e8c

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,12 @@
 class Dog
-
+    
+    attr_accessor :name
+    attr_reader :breed
+    def name=(name)
+        @name = name
+    end
+    
+    def breed=(breed)
+        @breed = breed
+    end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,11 @@
 class Person
+    attr_reader :name
+    attr_accessor :job
+    def name=(name)
+        @name = name
+    end
 
+    def job=(job)
+        @job = job
+    end
 end
